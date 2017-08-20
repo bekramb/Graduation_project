bd514e33695d592fa2dfa36bdec6ec64867c0c72
bekramb <bekramb@gmail.com>
2017-08-21 01:37:38 +0300

    Add Dish and Restraunt in model

---
+++ src/main/java/ru/svidersky/lunchproject/model/Restaurant.java
@@ -0,0 +1,28 @@
+package ru.svidersky.lunchproject.model;
+
+public class Restaurant {
+    private Integer id;
+
+    private String name;
+
+    public Restaurant(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
