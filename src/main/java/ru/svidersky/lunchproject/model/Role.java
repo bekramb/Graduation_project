130299c4b07ab2f97181ac60b4433eb222a7c7e4
bekramb <bekramb@gmail.com>
2017-08-21 01:20:47 +0300

    Initial commit

---
+++ src/main/java/ru/svidersky/lunchproject/model/Role.java
@@ -0,0 +1,6 @@
+package ru.svidersky.lunchproject.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/ru/svidersky/lunchproject/model/User.java
@@ -0,0 +1,92 @@
+package ru.svidersky.lunchproject.model;
+
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.Set;
+
+
+public class User  {
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private Date registered;
+
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(User u) {
+        this(u.getId(), u.getName(), u.getEmail(), u.getPassword(), u.getRoles());
+    }
+
+    public User(Integer id, String name, String email, String password, Role role, Role... roles) {
+        this(id, name, email, password, EnumSet.of(role, roles));
+    }
+
+    public User(Integer id, String name, String email, String password, Set<Role> roles) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + getId() +
+                ", email=" + email +
+                ", name=" + name +
+                '}';
+    }
+}
